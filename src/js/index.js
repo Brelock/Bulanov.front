5b8fe42275c73bd73c2b9cb2751dd75fd5441763
Mac <mac@MacBook-Pro-Mac.local>
2019-12-10 16:22:40 +0200

    first page

--- src/js/index.js
+++ src/js/index.js
@@ -165,11 +165,51 @@ $(document).ready(function() {
 			speed: 500,
 			fade: true,
 			cssEase: 'linear'
-			// проверка на ширину экрана
-	
+		});
+
+		$('.slide').slick({ 
+			// dots: true,
+			// infinite: true,
+			// speed: 500,
+			// fade: true,
+			// cssEase: 'linear',
+			// column: 2,
+			// slidesToShow: 2,
+			// slidesToScroll: 2,
+			dots: true,
+			infinite: false,
+			speed: 500,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			responsive: [
+			  {
+				breakpoint: 1024,
+				settings: {
+				  slidesToShow: 3,
+				  slidesToScroll: 3,
+				  infinite: true,
+				  dots: true
+				}
+			  },
+			  {
+				breakpoint: 600,
+				settings: {
+				  slidesToShow: 2,
+				  slidesToScroll: 2
+				}
+			  },
+			  {
+				breakpoint: 480,
+				settings: {
+				  slidesToShow: 1,
+				  slidesToScroll: 1
+				}
+			  }
+			]
 		});
 	}
 	
+
 
 });
 
